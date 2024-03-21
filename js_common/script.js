2dee0f17bb9d353660f735a558c75da92c70233b
Andriy <frobeniusfg@gmail.com>
2024-03-21 19:30:42 +0200

    simplification

--- js_common/script.js
+++ js_common/script.js
@@ -1,7 +1,7 @@
        
-let tabs_div      = document.getElementsByClassName("tabs-div")[0];
-let question_div  = document.getElementsByClassName("content-div")[0];
-let buttons_div   = document.getElementsByClassName("buttons-div")[0];
+let tabs_div;
+let container_div;
+let buttons_div;
 
 class QuizContainer
 {
@@ -171,7 +171,7 @@ class QuizContentMaker
 
   create_content(i)
   {
-    question_div.replaceChildren();
+    container_div.replaceChildren();
     let question_text = document.createElement("div");
     let delimeter = document.createElement("br");
     question_text.innerHTML = content_container.questions[i].question;
@@ -189,10 +189,10 @@ class QuizContentMaker
         item.style.background = content_container.answers[i][j] ? "lightgray" : "white";
         options_div.appendChild(item);
     }
-    question_div.appendChild(question_text);
-    question_div.appendChild(delimeter);
-    question_div.appendChild(options_div);
-    question_div.appendChild(delimeter.cloneNode(true));
+    container_div.appendChild(question_text);
+    container_div.appendChild(delimeter);
+    container_div.appendChild(options_div);
+    container_div.appendChild(delimeter.cloneNode(true));
     let submit_btn = document.createElement("button");
     submit_btn.className = "submit-btn btn"
     submit_btn.innerHTML = "Submit";
@@ -204,7 +204,7 @@ class QuizContentMaker
           result &= (content_container.questions[sm.slide].options[j].is_correct === content_container.answers[sm.slide][j]);
       sm.mark_slide(i, result ? "rgb(159, 250, 159)" : "rgb(255, 176, 176)");
     });
-    question_div.appendChild(submit_btn);
+    container_div.appendChild(submit_btn);
   }
 }
 
@@ -219,14 +219,30 @@ class HTMLContentMaker
 
   create_content(i)
   {
-    question_div.innerHTML = content_container.htmls[i];
+    container_div.innerHTML = content_container.htmls[i];
   }
+}
+
+function init_html_markup(){
+  let tabs_button_div = document.createElement("div");
+  tabs_button_div.className = "tabs-buttons-div"; 
+  tabs_div = document.createElement("div");
+  tabs_div.className = "tabs-div"; 
+  buttons_div      = document.createElement("div");
+  buttons_div.className = "buttons-div";
+  tabs_button_div.appendChild(tabs_div);
+  tabs_button_div.appendChild(buttons_div);
+  content_div      = document.createElement("div");
+  content_div.className = "content_div";
+  document.body.appendChild(tabs_button_div);
+  document.body.appendChild(content_div);
 }
 
 async function load_quiz_from(url) { 
   try{
     let data = await fetch(url);
     let file = await data.text();
+    init_html_markup();
     content_container = new QuizContainer(file);
     slide_manager     = new SlideManager(content_container.length());
     content_maker     = new QuizContentMaker();
@@ -248,6 +264,7 @@ async function load_quiz_from(url) {
 }
 
 async function load_slides_from(htmls) { 
+    init_html_markup();
     content_container = new HTMLContainer(htmls);
     slide_manager     = new SlideManager(content_container.length());
     content_maker     = new HTMLContentMaker();
