6cfbdeed292350740265b64210eb65d818343785
Vitalii Tymchyshyn <yu.binkukoku@gmail.com>
2025-09-05 02:33:45 +0300

    update pytutor for python 3.12+

--- pytutor/pg_logger.py
+++ pytutor/pg_logger.py
@@ -31,7 +31,7 @@
 # NB: try to import the minimal amount of stuff in this module to lessen
 # the security attack surface
 
-import imp
+import importlib
 import sys
 import bdb # the KEY import here!
 import re
@@ -76,7 +76,7 @@ PYTUTOR_HIDE_STR = '#pythontutor_hide:'
 # - also accepts shell globs, just like PYTUTOR_HIDE_STR
 PYTUTOR_INLINE_TYPE_STR = '#pythontutor_hide_type:'
 
-CLASS_RE = re.compile('class\s+')
+CLASS_RE = re.compile(r'class\s+')
 
 # copied-pasted from translate() in https://github.com/python/cpython/blob/2.7/Lib/fnmatch.py
 def globToRegex(pat):
@@ -113,7 +113,7 @@ def globToRegex(pat):
                 res = '%s[%s]' % (res, stuff)
         else:
             res = res + re.escape(c)
-    return res + '\Z(?ms)'
+    return res + r'\Z(?ms)'
 
 def compileGlobMatch(pattern):
     # very important to use match and *not* search!
